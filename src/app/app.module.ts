742f5697babc5d6a0d2f181bc40a1b6cf0e08e05
Croma4l <jonas.donnet@gmail.com>
2017-04-11 14:40:30 +0200

    Modification structure app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent }  from './app.component';
 import {EmailComponent } from './email/email.component';
-
+import {ImgComponent } from './scene/img.component';
+import {TxtComponent } from './scene/txt.component';
 @NgModule({
   imports:      [ BrowserModule , NgbModule.forRoot() ],
   declarations: [ AppComponent, EmailComponent ],
