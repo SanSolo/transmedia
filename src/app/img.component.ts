7843669499f0aad0a8f52737ae9b288e1d277dad
Croma4l <jonas.donnet@gmail.com>
2017-04-11 10:03:37 +0200

    Ajout du component pour les imgs

---
+++ src/app/img.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'img-screen',
+  template: `<img alt="{img-desc}" src="{img-src}">`,
+})
+export class AppComponent  { name = 'Jonas'; }
