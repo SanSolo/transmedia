8797abb62b49fc33765414058e1cd2bdfab20af6
Nils Lehmann <lehmannnil@gmail.com>
2017-04-11 11:41:09 +0200

    Added bootstrap dependicies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello {{name}}</h1><p>
+  <ngb-alert [dismissible]="false">
+    <strong>Warning!</strong> Better check yourself, you're not looking too good.
+  </ngb-alert>
+</p>`,
 })
 export class AppComponent  { name = 'Angular'; }
