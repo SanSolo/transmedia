ac28ee394aae8897f85f8ade3a9fa5a8f1eab853
Croma4l <jonas.donnet@gmail.com>
2017-04-28 15:00:11 +0200

    modif proxy

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express = require('express')
-var proxy = require('express-http-proxy');
 
 var app = express();
+var proxy = require('http-proxy-middleware');
+
+var apiProxy = proxy('/mailchimp', {
+    target: 'https://us15.api.mailchimp.com/3.0/lists/eb7b58de02/members',
+    changeOrigin: true,  // for vhosted sites
+    pathRewrite: {"^/mailchimp": ""}
+});
  
-app.use('/mailchimp', proxy('https://us15.api.mailchimp.com/3.0/lists/eb7b58de02/members'));
+app.use(apiProxy);
 
 app.use(express.static('src'));
 
@@ -14,5 +20,5 @@ app.get('/*', function(req, res) {
 });
 
 app.listen(process.env.PORT || 3015, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3015!')
 });
