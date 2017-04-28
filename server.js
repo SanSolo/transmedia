d250948f3343558af62f73dfc1db85c2e9bb20e9
Croma4l <jonas.donnet@gmail.com>
2017-04-28 11:35:03 +0200

    modif express

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var proxy = require('express-http-proxy');
+
+var app = express();
+ 
+app.use('/mailchimp', proxy('https://us15.api.mailchimp.com/3.0/lists/eb7b58de02/members'));
+
+app.use(express.static('src'));
+
+app.use('/node_modules', express.static('node_modules'));
+
+app.get('/*', function(req, res) {
+  res.sendFile('src/index.html', { root: __dirname });
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Example app listening on port 3000!')
+});
